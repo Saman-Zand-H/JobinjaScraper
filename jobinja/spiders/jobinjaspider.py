501fb40af356ce04b72dcc277f84e3d4b288fe06
DJS <tnsperuse@gmail.com>
2023-08-10 18:09:26 +0330

    init commit

---
+++ jobinja/spiders/jobinjaspider.py
@@ -0,0 +1,40 @@
+import scrapy
+from urllib.parse import parse_qs
+from logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+class JobinjaspiderSpider(scrapy.Spider):
+    name = "jobinjaspider"
+    allowed_domains = ["jobinja.ir"]
+    # this is a custom attr that let's us choose number of paginated pages we want to scrape
+    number_of_pages = 50
+    start_urls = ["https://jobinja.ir/jobs/category/it-software-web-development-jobs/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85-%D9%88%D8%A8-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87-%D9%86%D9%88%DB%8C%D8%B3-%D9%86%D8%B1%D9%85-%D8%A7%D9%81%D8%B2%D8%A7%D8%B1?preferred_before=1691671654&sort_by=relevance_desc"]
+
+    def parse(self, response):
+        job_links = response.xpath('//*[@id="js-jobSeekerSearchResult"]/div/div[3]/section/div/ul/li/div/div[2]/a/@href').getall()
+        for job_link in job_links:
+            yield scrapy.Request(
+                job_link,
+                callback=self.parse_job_link
+            )
+        
+        next_page = response.css('a[rel="next"] ::attr(href)').get()
+        page_num: list = parse_qs(next_page).get("page", [])
+        if len(page_num) <= 0:
+            return
+        page_num: int = int(page_num[0])
+        
+        if next_page is not None and page_num <= self.number_of_pages:
+            yield response.follow(
+                next_page,
+                callback=self.parse
+            )
+
+    def parse_job_link(self, response):
+        techs = response.xpath('//*[contains(@class, "c-infoBox") and contains(@class, "u-mB0")]//li[contains(@class, "c-infoBox__item")][1]/div[contains(@class, "tags")]/span/text()').getall()
+        logger.error(techs)
+        for tech in techs:
+            yield {"name": tech.strip().lower()}
